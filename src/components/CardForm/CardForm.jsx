4b7744cb721bdcd70459eee781d2fda780e7df70
gabimdev <gabrielmlima@gmail.com>
2021-11-22 22:02:26 +0100

    CONFIG: add Bootstrap

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -1,5 +1,9 @@
 const CardForm = () => {
-    return <div>CardForm</div>;
+    return (
+        <div className="d-flex align-items-center mb-3 btn-primary">
+            CardForm
+        </div>
+    );
 };
 
 export default CardForm;
