d66b95bc5fd90dce67834a95db594719d331a94e
gabimdev <gabrielmlima@gmail.com>
2021-11-23 20:08:11 +0100

    FEAT: footer finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,23 @@
 import './Footer.scss';
+import redSys from '../../assets/image5.png';
 
 const Footer = () => {
-    return <div>footer</div>;
+    return (
+        <div className="footer">
+            <span>Powered by</span>
+            <span>
+                <img src={redSys} alt="redSysLogo" />
+            </span>
+            <div>
+                <a href className="footer__link">
+                    Terms
+                </a>
+                <a href className="footer__link">
+                    Privacy
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
