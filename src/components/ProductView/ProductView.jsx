217947d53516a056532cc0a0714de46c26825141
gabimdev <gabrielmlima@gmail.com>
2021-11-27 12:27:48 +0100

    STYLE: refactor styles at ProductView compnent. Substituted flex by grid

--- src/components/ProductView/ProductView.jsx
+++ src/components/ProductView/ProductView.jsx
@@ -5,22 +5,29 @@ const ProductView = () => {
     const img = require('../../assets/image7.png').default;
 
     return (
-        <div className="product-container">
-            <div className="product-container__image-title">
-                <img
-                    src={img}
-                    alt="product name"
-                    className="product-container__img"
-                />
+        <div className="row justify-content-center align-items-center">
+            <div className="row-12 justify-content-center align-items-center order-1 order-md-2">
+                <img src={img} alt="product name" className="col-6 col-md-8" />
             </div>
-            <div className="product-container__text">
-                <p className="product-container__title">{getIten.name}</p>
-                <div className="product-container-flex">
-                    <p className="product-container__price">$</p>
-                    <p className="product-container__price margin">
+            <div
+                className="col-6
+            justify-content-center
+            margin-title-box
+            order-2
+            order-md-1
+            justify-content-md-start"
+            >
+                <div className="col-12 col-md-6 col-lg-4   justify-content-center">
+                    <span className="product-title col-auto">
+                        {getIten.name}
+                    </span>
+                </div>
+                <div className="col-12 margin-price-box  col-md-10 col-lg-6">
+                    <span className="product-price col-2 m-0">$</span>
+                    <span className="product-price margin col-10 m-0">
                         {' '}
                         {getIten.price}
-                    </p>
+                    </span>
                 </div>
             </div>
         </div>
