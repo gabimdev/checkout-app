0746e5dd773ffd9dfe6edbe092af861ce0d46601
gabimdev <gabrielmlima@gmail.com>
2021-11-28 09:50:15 +0100

    FEAT: add try catch to coutnry.js call api

--- src/api/country.js
+++ src/api/country.js
@@ -1,13 +1,17 @@
 const apiDirection = 'https://countriesnow.space/api/v0.1';
-const allCountrys = `${apiDirection}/countries/flag/unicode`;
+const allCountrys = `${apiDirection}/countries/flag/images`;
 
 export const getAllCountries = async () => {
-    const request = await fetch(allCountrys, {
-        method: 'GET',
-    });
-    const response = await request.json();
-    if (!request.ok) {
-        throw new Error(response.message);
+    try {
+        const request = await fetch(allCountrys, {
+            method: 'GET',
+        });
+        const response = await request.json();
+        if (!request.ok) {
+            throw new Error(response.message);
+        }
+        return response;
+    } catch (error) {
+        console.log(error.message);
     }
-    return response;
 };
